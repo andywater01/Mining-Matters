5ae32f8c5fdce4734d99fecfdb965c662200c083
Andy Waterhouse <andrew.waterhouse@ontariotechu.net>
2023-01-18 17:35:07 -0500

    Camera Look Movement
    implemented camera look

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector2 turn;
+    public float sensitivity = .5f;
+    public Vector3 deltaMove;
+    public float speed = 1;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+
+        turn.x = Mathf.Clamp(turn.x, -60f, 60f);
+        turn.y = Mathf.Clamp(turn.y, -40f, 30f);
+
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+}
